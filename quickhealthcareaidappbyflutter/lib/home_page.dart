92f8cf5ee15b033f9f475504491e2c718ae9852b
eoao <mdarscht@stud.hs-heilbronn.de>
2020-06-05 00:43:42 +0200

    ed is my friend

--- quickhealthcareaidappbyflutter/lib/home_page.dart
+++ quickhealthcareaidappbyflutter/lib/home_page.dart
@@ -10,31 +10,36 @@ import 'package:quickhealthcareaidappbyflutter/services/places_service.dart';
 //void main() => runApp(MyApp());
 
 class HomePage extends StatelessWidget {
-  final locatorService = GeoLocatorService(); 
+  final locatorService = GeoLocatorService();
   final placesService = PlacesService();
 
   @override
-  Widget build(BuildContext context) {  
+  Widget build(BuildContext context) {
     return MultiProvider(
-        providers:[
-          FutureProvider(create: (context)=> locatorService.getLocation()),
-          FutureProvider(create: (context){
-            ImageConfiguration configuration = createLocalImageConfiguration(context);
-            return BitmapDescriptor.fromAssetImage(configuration, 'assets/images/hospital.png');
-          }),
-          ProxyProvider2<Position,BitmapDescriptor,Future<List<Place>>>(
-            update:(context,position,icon,places){
-              return (position != null) ? placesService.getPlaces(position.latitude, position.longitude,icon) :null;
-            } ,)
-        ],
-         child: MaterialApp(
+      providers: [
+        FutureProvider(create: (context) => locatorService.getLocation()),
+        FutureProvider(create: (context) async {
+          ImageConfiguration configuration =
+              createLocalImageConfiguration(context);
+          return BitmapDescriptor.fromAssetImage(
+              configuration, 'assets/images/hospital.png');
+        }),
+        ProxyProvider2<Position, BitmapDescriptor, Future<List<Place>>>(
+          update: (context, position, icon, places) {
+            return (position != null)
+                ? placesService.getPlaces(
+                    position.latitude, position.longitude, icon)
+                : null;
+          },
+        )
+      ],
+      child: MaterialApp(
         title: 'Quick Health Care Aid App',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home:Search(),
+        home: Search(),
       ),
     );
   }
 }
-
--- quickhealthcareaidappbyflutter/lib/main.dart
+++ quickhealthcareaidappbyflutter/lib/main.dart
@@ -1,18 +1,19 @@
 import 'dart:async';
-import 'home_page.dart';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'home_page.dart';
+
 void main() {
   runApp(MaterialApp(
     theme:
-      ThemeData(primaryColor: Colors.red, accentColor: Colors.yellowAccent),
-      debugShowCheckedModeBanner: false,
-      home: SplashScreen(), 
-    )
-  );
+        ThemeData(primaryColor: Colors.red, accentColor: Colors.yellowAccent),
+    debugShowCheckedModeBanner: false,
+    home: SplashScreen(),
+  ));
 }
+
 class SplashScreen extends StatefulWidget {
   @override
   _SplashScreenState createState() => _SplashScreenState();
@@ -20,72 +21,79 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    Timer(Duration(seconds: 6),(){
-      Navigator.push(context, MaterialPageRoute(builder: (context) =>HomePage(), 
-        ),
-      );
+    setState(() {
+      Timer(Duration(seconds: 6), () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => HomePage(),
+          ),
+        );
+      });
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        fit:StackFit.expand,
-        children:<Widget>[
-          Container(
-            decoration: BoxDecoration(color: Colors.redAccent),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children:<Widget>[
-              Expanded(
-                flex: 2,
-                child: Container(
-                  child:Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      CircleAvatar(
-                        backgroundColor: Colors.white,
-                        radius: 50.0,
-                        child: Icon(
-                          Icons.local_hospital,
-                          color: Colors.redAccent,
-                          size: 50.0,
-                        ),
-                        ),
-                        Padding(padding: EdgeInsets.only(top:10.0),
-                        ),
-                        Text(
-                          "Quick Health Care /n Aid App",
-                          style:TextStyle(
-                            color:Colors.white,
-                            fontSize:24,
-                            fontWeight: FontWeight.bold),
-                            )
-                    ],
-                  ),
-                ) ,
+      body: Stack(fit: StackFit.expand, children: <Widget>[
+        Container(
+          decoration: BoxDecoration(color: Colors.redAccent),
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Expanded(
+              flex: 2,
+              child: Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    CircleAvatar(
+                      backgroundColor: Colors.white,
+                      radius: 50.0,
+                      child: Icon(
+                        Icons.local_hospital,
+                        color: Colors.redAccent,
+                        size: 50.0,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 10.0),
+                    ),
+                    Text(
+                      "Quick Health Care /n Aid App",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold),
+                    )
+                  ],
                 ),
-               Expanded(
-                 flex: 1,
-                 child: Column(
-                   mainAxisAlignment:MainAxisAlignment.center,
-                   children:<Widget>[
-                     CircularProgressIndicator(),
-                     Padding(padding: EdgeInsets.only(top: 20.0),
-                     ),
-                     Text("The Quickest Tracker to \n  your nearest hospital",style: TextStyle(
-                       color: Colors.white,
-                       fontSize: 18.0,
-                       fontWeight: FontWeight.bold)),
-                   ],
-                 )  ,
-               ) 
-            ],)
-        ]
-      ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CircularProgressIndicator(),
+                  Padding(
+                    padding: EdgeInsets.only(top: 20.0),
+                  ),
+                  Text("The Quickest Tracker to \n  your nearest hospital",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold)),
+                ],
+              ),
+            )
+          ],
+        )
+      ]),
     );
   }
 }

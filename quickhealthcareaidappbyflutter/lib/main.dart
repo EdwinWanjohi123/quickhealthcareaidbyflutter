02a3dcb3cc6324f0471a7ffa433fa2cbc705cb11
eoao <mdarscht@stud.hs-heilbronn.de>
2020-06-05 01:14:44 +0200

    updating animation

--- quickhealthcareaidappbyflutter/lib/main.dart
+++ quickhealthcareaidappbyflutter/lib/main.dart
@@ -20,80 +20,87 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  bool _visible = false;
+
   @override
   void initState() {
     super.initState();
-    setState(() {
-      Timer(Duration(seconds: 6), () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => HomePage(),
-          ),
-        );
-      });
+    Timer(Duration(seconds: 3), () {
+      _visible = true;
+    });
+    Timer(Duration(seconds: 6), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => HomePage(),
+        ),
+      );
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(fit: StackFit.expand, children: <Widget>[
-        Container(
-          decoration: BoxDecoration(color: Colors.redAccent),
-        ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Expanded(
-              flex: 2,
-              child: Container(
+    return AnimatedOpacity(
+      opacity: !_visible ? 1.0 : 0.0,
+      duration: Duration(seconds: 2),
+      child: Scaffold(
+        body: Stack(fit: StackFit.expand, children: <Widget>[
+          Container(
+            decoration: BoxDecoration(color: Colors.redAccent),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      CircleAvatar(
+                        backgroundColor: Colors.white,
+                        radius: 50.0,
+                        child: Icon(
+                          Icons.local_hospital,
+                          color: Colors.redAccent,
+                          size: 50.0,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 10.0),
+                      ),
+                      Text(
+                        "Quick Health Care /n Aid App",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    CircleAvatar(
-                      backgroundColor: Colors.white,
-                      radius: 50.0,
-                      child: Icon(
-                        Icons.local_hospital,
-                        color: Colors.redAccent,
-                        size: 50.0,
-                      ),
-                    ),
+                    CircularProgressIndicator(),
                     Padding(
-                      padding: EdgeInsets.only(top: 10.0),
+                      padding: EdgeInsets.only(top: 20.0),
                     ),
-                    Text(
-                      "Quick Health Care /n Aid App",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold),
-                    )
+                    Text("The Quickest Tracker to \n  your nearest hospital",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold)),
                   ],
                 ),
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  CircularProgressIndicator(),
-                  Padding(
-                    padding: EdgeInsets.only(top: 20.0),
-                  ),
-                  Text("The Quickest Tracker to \n  your nearest hospital",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.bold)),
-                ],
-              ),
-            )
-          ],
-        )
-      ]),
+              )
+            ],
+          )
+        ]),
+      ),
     );
   }
 }
